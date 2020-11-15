e301c3f02246b507545c09d8ddfe261eb2711035
Zafer CÖMERT <zafer.comert@hotmail.com>
2020-11-15 20:28:22 +0300

    Seri has been created.

--- Matematik/Seri.cs
+++ Matematik/Seri.cs
@@ -8,6 +8,31 @@ namespace Matematik
 {
     public class Seri
     {
+        // Serinin değerlerini tutar. 
+        public List<int> S { get; set; }
+        
+        // Belirli sayılardan bir seri oluşturur. 
+        public Seri(params int[] s)
+        {
+            S = new List<int>();
+            foreach (var item in s)
+                S.Add(item);
+        }
 
+        /// <summary>
+        /// Seri toplamini hesaplar. 
+        /// </summary>
+        /// <returns></returns>
+        public int Toplam()
+        {
+            // toplami turar. 
+            int toplam = 0;
+
+            // toplami hesaplar.
+            foreach (var item in S)
+                toplam += item;
+            // toplami doner
+            return toplam;
+        }
     }
 }
